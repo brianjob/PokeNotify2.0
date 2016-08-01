96d6e02628a474b0685464ca7b8a7f281a8ed45a
Brian Barton <brianjob@umich.edu>
2016-08-01 00:02:40 -0400

    make time_left_in_seconds available to module

--- utils.js
+++ utils.js
@@ -1,8 +1,9 @@
   var moment = require('moment');
 
-  module.exports.time_left_in_seconds = function(despawn_timestamp) {
+ var time_left_in_seconds = function(despawn_timestamp) {
     return (get_expiration_date(despawn_timestamp) - moment()) / 1000;
   };
+  module.exports.time_left_in_seconds = time_left_in_seconds;
 
   module.exports.get_expiration_date = function(despawn_timestamp) {
     return moment.unix(despawn_timestamp).subtract(new Date().getTimezoneOffset(), 'minutes');
