46a1fe109690a2f0a9c09809eac0cda2523f8b7c
Brian Barton <brianjob@umich.edu>
2016-08-01 00:03:56 -0400

    it's getting late apparently

--- utils.js
+++ utils.js
@@ -5,6 +5,7 @@
   };
   module.exports.time_left_in_seconds = time_left_in_seconds;
 
-  module.exports.get_expiration_date = function(despawn_timestamp) {
+  var get_expiration_date = function(despawn_timestamp) {
     return moment.unix(despawn_timestamp).subtract(new Date().getTimezoneOffset(), 'minutes');
   };
+  module.exports.get_expiration_date = get_expiration_date;
